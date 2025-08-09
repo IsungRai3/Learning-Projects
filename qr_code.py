135d55b1cbe3277cffa2c094ef72290c964d432b
Isung rai <raiisung9@gmail.com>
2025-08-09 21:48:43 +0545

    more to come

---
+++ qr_code.py
@@ -0,0 +1,8 @@
+import qrcode
+
+qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+qr.add_data("https://www.youtube.com/watch?v=fJ9rUzIMcZQ&list=RDKEvXoPFi28k&index=16")
+qr.make()
+
+img = qr.make_image(fill_color = "black", back_color = "white" )
+img.save('youtube_qr.png')
