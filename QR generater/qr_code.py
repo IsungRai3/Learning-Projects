fe9e8094e21ca30e3ce66a9bdef08cdaaee01aae
Isung rai <raiisung9@gmail.com>
2025-09-16 20:11:58 +0545

    just learn

--- QR generater/qr_code.py
+++ QR generater/qr_code.py
@@ -1,5 +1,8 @@
 import qrcode
 
+#User input
+url = input("Enter the link : ")
+
 qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
 qr.add_data("https://www.youtube.com/watch?v=fJ9rUzIMcZQ&list=RDKEvXoPFi28k&index=16")
 qr.make()
